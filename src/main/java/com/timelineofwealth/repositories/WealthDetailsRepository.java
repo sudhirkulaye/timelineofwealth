cb38203342eacb71e0ffc4f22b7e76efaafeb595
sudhir <sudhir.kulaye@gmail.com>
2018-12-23 14:19:52 +0530

    20181223_1

--- src/main/java/com/timelineofwealth/repositories/WealthDetailsRepository.java
+++ src/main/java/com/timelineofwealth/repositories/WealthDetailsRepository.java
@@ -10,8 +10,8 @@ import java.util.List;
 public interface WealthDetailsRepository extends JpaRepository<WealthDetails, WealthDetails.WealthDetailsKey> {
 
     public List<WealthDetails> findByKeyMemberidInOrderByKeyMemberidAscAssetClassidAscKeyTickerAscKeyBuyDateAsc(List<Long> memberids);
-    @Query("SELECT ticker, sum(quantity), sum( ")
-    public List<WealthDetails> findByKeyMemberidInGroupByKeyMemberidAndKeyTicker(List<Long> memberids);
+    //@Query("SELECT ticker, sum(quantity), sum( ")
+    //public List<WealthDetails> findByKeyMemberidInGroupByKeyMemberidAndKeyTicker(List<Long> memberids);
     public int countByKeyMemberidAndKeyBuyDateAndKeyTicker(long memberid, Date buyDate, String ticker);
     public WealthDetails findByKeyMemberidAndKeyBuyDateAndKeyTicker(long memberid, Date buyDate, String ticker);
     public List<WealthDetails>  findByKeyMemberidAndKeyTicker(long memberid, String ticker);
